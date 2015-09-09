43dedef2c7663bafc2245d28b9aa9bf32e4c779a
Ryan Nowakowski <tubaman@cuisine.keyingredient.com>
2015-09-09 15:11:48 -0500

    initial

---
+++ TinyShield_NRF8001_BLE_Example.ino
@@ -0,0 +1,33 @@
+#define BLE_DEBUG 1
+
+#include "SPI.h"
+#include "lib_aci.h"
+#include "aci_setup.h"
+#include "uart_over_ble.h"
+#include "services.h"
+
+uint8_t ble_rx_buffer[21];
+uint8_t ble_rx_buffer_len = 0;
+
+//when using this project in the Arduino IDE, delete the following include and rename UART.h to UART.ino
+#include "UART.h"
+
+void setup(void)
+{
+  Serial.begin(115200);
+  BLEsetup();
+}
+
+void loop() {
+  aci_loop();//Process any ACI commands or events
+  
+  if(ble_rx_buffer_len){
+    Serial.println(ble_rx_buffer_len);
+    Serial.println((char*)ble_rx_buffer);
+    uint8_t sendBuffer[20]="Test";
+    uint8_t length=5;
+    lib_aci_send_data(PIPE_UART_OVER_BTLE_UART_TX_TX, sendBuffer, length);
+    ble_rx_buffer_len=0;
+  }
+
+}
